d0105058956b9ec620ee080b19917ba67cd65af9
Luke <przezorski@gmail.com>
2016-12-23 20:14:15 +0100

    Update chewieConfig.js

--- chewieConfig.js
+++ chewieConfig.js
@@ -10,7 +10,7 @@ const config = {
 
   registry: {
     location: process.env.REGISTRY_LOCATION || 'remote',
-    path: process.env.REGISTRY_PATH || 'https://'+$GH_TOKEN+'@github.com/przezor/docpad-skeleton-apidocs-sampledata.git',
+    path: process.env.REGISTRY_PATH || 'https://github.com/przezor/docpad-skeleton-apidocs-sampledata.git',
     fileName: 'docu_registry.json',
     branch: process.env.docuBranch || 'master',
     shortVersionFileName: 'shrinkedRegistry.json',
@@ -30,7 +30,7 @@ const config = {
   },
 
   generationResult: {
-    srcLocation: 'https://'+$GH_TOKEN+'@github.com/przezor/chewie-sample-result.git',
+    srcLocation: 'https://github.com/przezor/chewie-sample-result.git',
     branch: process.env.docuBranch || 'master',
     cloneLocation: 'latestResultRepo'
   },
